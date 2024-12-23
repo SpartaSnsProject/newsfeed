aa281d4e3b708855116868eadd763a4e25078300
wldnr1208@naver.com <wldnr1208@naver.com>
2024-12-23 15:55:30 +0900

    feat: signUp done

--- src/main/java/com/example/newsfeed/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/newsfeed/exception/GlobalExceptionHandler.java
@@ -1,14 +1,20 @@
 package com.example.newsfeed.exception;
+
 import com.example.newsfeed.dto.common.ApiResponse;
 import com.example.newsfeed.exception.user.DuplicateEmailException;
 import com.example.newsfeed.exception.user.DuplicateUsernameException;
 import lombok.extern.slf4j.Slf4j;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
 
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
