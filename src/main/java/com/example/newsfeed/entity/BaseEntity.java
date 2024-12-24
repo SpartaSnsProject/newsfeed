a1b7e580b289c320698764d85be363656775d6db
afkorigin <gnstldgk@naver.com>
2024-12-24 15:55:43 +0900

    fix entity

--- src/main/java/com/example/newsfeed/entity/BaseEntity.java
+++ src/main/java/com/example/newsfeed/entity/BaseEntity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.newsfeed.entity;
 
 import jakarta.persistence.Column;
@@ -22,28 +21,3 @@ public class BaseEntity {
     @LastModifiedDate
     private LocalDateTime modifiedAt;
 }
-=======
-package com.example.newsfeed.entity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-
-@Getter
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime modifiedAt;
-}
->>>>>>> dev
